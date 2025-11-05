27629cb99bf5d55a72f8c9b45514e1060a486eca
ebytom <ebytomy7@gmail.com>
2025-11-05 10:23:36 +0530

    routing and navigations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React, { useState } from "react";
+import "./App.css";
+import Routes from "./Routes/Routes";
+// import { ToastProvider } from "./Components/ToastContext/ToastContext";
+// import { WarrantyProvider } from "./Components/WarrantyContext/WarrantyContext";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+export const UserContext = React.createContext();
 
 function App() {
+  const [user, setUser] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex App">
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_URL}>
+        <UserContext.Provider value={{ user, setUser }}>
+          {/* <ToastProvider> */}
+            {/* <WarrantyProvider> */}
+              <Routes />
+            {/* </WarrantyProvider> */}
+          {/* </ToastProvider> */}
+        </UserContext.Provider>
+      </GoogleOAuthProvider>
     </div>
   );
 }
